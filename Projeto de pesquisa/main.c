7ef23159197c617e4ea266c14ee1f578dce8f1ea
Maria Francisca <mariafranciscaflor73@hotmail.com>
2024-09-06 21:12:46 -0300

    Update main.c
    Atualizado o código

--- Projeto de pesquisa/main.c
+++ Projeto de pesquisa/main.c
@@ -1,6 +1,15 @@
 #include "sistema.h"
 
 int main(){
+    int total = 50;
+    Dados *dados = alocar_dados(total);
+    if (dados == NULL) {
+        exit(1); 
+    }
+
+    adicionar_projeto(dados);
+
+    free(dados);
 
     return 0;
 }
