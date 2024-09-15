b93b8f208e0bf01998c21640b5ad32043be1a80c
Maria Francisca <mariafranciscaflor73@hotmail.com>
2024-09-15 18:36:07 -0300

    Update main.c

--- Projeto de pesquisa/main.c
+++ Projeto de pesquisa/main.c
@@ -1,15 +1,46 @@
 #include "sistema.h"
 
-int main(){
-    int total = 50;
-    Dados *dados = alocar_dados(total);
-    if (dados == NULL) {
-        exit(1); 
-    }
+int main() {
+    No *lista = NULL;
+    int opcao, codigo_para_excluir;
 
-    adicionar_projeto(dados);
+    do {
+        printf("\nDigite uma opção:\n");
+        printf("1. Adicionar projeto\n");
+        printf("2. Imprimir Projeto\n");
+        printf("3. Excluir projeto\n");
+        printf("4. Sair\n");
+        printf("Opção: ");
+        scanf("%d", &opcao);
 
-    free(dados);
+        switch (opcao) {
+            case 1:
+                adicionar_projeto(&lista);
+                break;
 
+            case 2:
+                imprimir_projeto(lista);
+                break;
+
+            case 3:
+                if (lista == NULL) {
+                    printf("A lista está vazia.\n");
+                } else {
+                    printf("Digite o código do projeto para excluir: ");
+                    scanf("%d", &codigo_para_excluir);
+                    excluir_projeto(&lista, codigo_para_excluir);
+                }
+                break;
+
+            case 4:
+                liberar_lista(lista);
+                printf("Saindo...\n");
+                break;
+
+            default:
+                printf("Opção inválida. Tente novamente.\n");
+                break;
+        }
+    } while (opcao != 4);     
     return 0;
 }
