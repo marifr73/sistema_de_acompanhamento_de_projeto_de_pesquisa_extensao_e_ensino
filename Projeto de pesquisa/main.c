68f8017e056031e4d60d0ac12ad9128f94c12773
Maria Francisca <mariafranciscaflor73@hotmail.com>
2024-09-17 13:46:41 -0300

    Update main.c

--- Projeto de pesquisa/main.c
+++ Projeto de pesquisa/main.c
@@ -1,38 +1,50 @@
 #include "sistema.h"
 
-int main() {
+int main(){
     No *lista = NULL;
     int opcao, codigo_para_excluir;
+    char *nome_arquivo = "projetos.txt";
+
+    ler_arquivo(&lista, nome_arquivo);
 
     do {
         printf("\nDigite uma opção:\n");
         printf("1. Adicionar projeto\n");
-        printf("2. Imprimir Projeto\n");
-        printf("3. Excluir projeto\n");
-        printf("4. Sair\n");
+        printf("2. Listar projetos\n");
+        printf("3. Imprimir projeto\n");
+        printf("4. Excluir projeto\n");
+        printf("5. Sair\n");
         printf("Opção: ");
         scanf("%d", &opcao);
+        getchar();
 
-        switch (opcao) {
+        switch (opcao){
             case 1:
                 adicionar_projeto(&lista);
+                abrir_arquivo(lista, nome_arquivo);
                 break;
 
             case 2:
-                imprimir_projeto(lista);
+                lista_projeto(lista);
                 break;
 
             case 3:
+                imprimir_projeto(lista);
+                break;
+
+            case 4:
                 if (lista == NULL) {
                     printf("A lista está vazia.\n");
                 } else {
                     printf("Digite o código do projeto para excluir: ");
                     scanf("%d", &codigo_para_excluir);
+                    getchar();
                     excluir_projeto(&lista, codigo_para_excluir);
+                    abrir_arquivo(lista, nome_arquivo);
                 }
                 break;
 
-            case 4:
+            case 5:
                 liberar_lista(lista);
                 printf("Saindo...\n");
                 break;
@@ -41,6 +53,7 @@ int main() {
                 printf("Opção inválida. Tente novamente.\n");
                 break;
         }
-    } while (opcao != 4);     
+    } while (opcao != 5);
+
     return 0;
 }
