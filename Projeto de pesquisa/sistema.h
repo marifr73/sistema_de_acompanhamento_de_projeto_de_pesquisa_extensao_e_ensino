5da7b25fcb101a6f60c2b07be11ad6e31818eb3f
Maria Francisca <mariafranciscaflor73@hotmail.com>
2024-09-17 13:48:34 -0300

    Update sistema.h

--- Projeto de pesquisa/sistema.h
+++ Projeto de pesquisa/sistema.h
@@ -1,6 +1,5 @@
 #ifndef SISTEMA_H  
 #define SISTEMA_H 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -13,16 +12,20 @@ typedef enum tipo Tipo;
 
 typedef enum situacao Situacao;
 
-typedef union nome Nome;
-
-No* alocar_no();
+typedef struct nome Nome;
 
 void adicionar_projeto(No **lista);
 
-void imprimir_projeto (No *lista);
+void imprimir_projeto(No *lista);
+
+void lista_projeto(No *lista);
 
 void excluir_projeto(No **lista, int codigo_identificador);
 
+void abrir_arquivo(No *lista, char *nome_arquivo);
+
+void ler_arquivo(No **lista, char *nome_arquivo);
+
 void liberar_lista(No *lista);
 
-#endif
+#endif //SISTEMA_H
