3a64d2dda9315876de20007035fb0554a1f2de3f
Maria Francisca <mariafranciscaflor73@hotmail.com>
2024-09-23 12:03:32 -0300

    Update main.c
    Add novas funcoes

--- Projeto de pesquisa/main.c
+++ Projeto de pesquisa/main.c
@@ -1,6 +1,6 @@
 #include "sistema.h"
 
-int main(){
+int main() {
     No *lista = NULL;
     int opcao, codigo_para_excluir;
     char *nome_arquivo = "projetos.txt";
@@ -11,49 +11,70 @@ int main(){
         printf("\nDigite uma opção:\n");
         printf("1. Adicionar projeto\n");
         printf("2. Listar projetos\n");
-        printf("3. Imprimir projeto\n");
-        printf("4. Excluir projeto\n");
-        printf("5. Sair\n");
-        printf("Opção: ");
-        scanf("%d", &opcao);
-        getchar();
+        printf("3. Consultar projeto por tipo\n");
+        printf("4. Consultar projeto por situação\n");
+        printf("5. Buscar projeto por código\n");
+        printf("6. Editar projeto\n");
+        printf("7. Excluir projeto\n");
+        printf("8. Sair\n");
 
-        switch (opcao){
+        if (scanf("%d", &opcao) != 1) {
+            printf("Opção invalida! Informe um número.\n");
+            while (getchar() != '\n'); 
+            continue;
+        }
+
+        switch (opcao) {
             case 1:
                 adicionar_projeto(&lista);
                 abrir_arquivo(lista, nome_arquivo);
                 break;
 
             case 2:
-                lista_projeto(lista);
+                listar_projeto(lista);
                 break;
 
             case 3:
-                imprimir_projeto(lista);
+                consultar_projeto_por_tipo(lista);
                 break;
 
             case 4:
+                consultar_projeto_por_situacao(lista);
+                break;
+            
+            case 5:
+                buscar_projeto_por_codigo(lista);
+                break;
+
+            case 6:
+                editar_dados_do_projeto(lista);
+                break;
+
+            case 7:
                 if (lista == NULL) {
                     printf("A lista está vazia.\n");
                 } else {
                     printf("Digite o código do projeto para excluir: ");
-                    scanf("%d", &codigo_para_excluir);
-                    getchar();
-                    excluir_projeto(&lista, codigo_para_excluir);
-                    abrir_arquivo(lista, nome_arquivo);
+                    if (scanf("%d", &codigo_para_excluir) == 1) {
+                        excluir_projeto(&lista, codigo_para_excluir);
+                        abrir_arquivo(lista, nome_arquivo);
+                    } else {
+                        printf("Código inválido!\n");
+                        while (getchar() != '\n');
+                    }
                 }
                 break;
 
-            case 5:
+            case 8:
                 liberar_lista(lista);
                 printf("Saindo...\n");
                 break;
 
             default:
-                printf("Opção inválida. Tente novamente.\n");
+                printf("Opção não cadastrada!\n");
                 break;
         }
-    } while (opcao != 5);
+    } while (opcao != 8);
 
     return 0;
 }
