6217dc17723f940dd2049952ed71bcb291c7bf65
Maria Francisca <mariafranciscaflor73@hotmail.com>
2024-09-23 12:04:59 -0300

    Update sistema.h

--- Projeto de pesquisa/sistema.h
+++ Projeto de pesquisa/sistema.h
@@ -16,15 +16,23 @@ typedef struct nome Nome;
 
 void adicionar_projeto(No **lista);
 
-void imprimir_projeto(No *lista);
-
-void lista_projeto(No *lista);
+void listar_projeto(No *lista);
 
 void excluir_projeto(No **lista, int codigo_identificador);
 
 void abrir_arquivo(No *lista, char *nome_arquivo);
 
 void ler_arquivo(No **lista, char *nome_arquivo);
+
+void consultar_projeto_por_tipo(No *lista);
+
+void consultar_projeto_por_situacao(No *lista);
+
+void listar_projeto_unico(No *projeto);
+
+void buscar_projeto_por_codigo(No *lista);
+
+void editar_dados_do_projeto(No *lista);
 
 void liberar_lista(No *lista);
 
